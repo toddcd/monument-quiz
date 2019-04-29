796e9d7047ad35ce5a372393f4bcb44f49053585
Todd Davis <tejastcd@gmail.com>
2019-04-29 14:56:26 -0400

    initial commit of momument quiz

---
+++ data.js
@@ -0,0 +1,103 @@
+const QUIZ_DATA = {
+    questions: [
+        {
+            id: 1,
+            text: "Which of the following bicycle races are considered a Monument?",
+            desc: "At the top of world tour cycling are 5 of the longest, hardest and most prestigious one-day races in professional cycling. These are considered the Monuments.",
+            choices:
+                [{text: "Milan-San Remo"},
+                    {text: "Tour of Flanders"},
+                    {text: "Paris-Roubaix"},
+                    {text: "Liege-Bastogne-Liege"},
+                    {text: "Giro di Lombardia"},
+                    {
+                        text: "All of the above.",
+                        isCorrect: true
+                    }
+                ]
+        },
+        {
+            id: 2,
+            text: "Which cycling Monument is the longest and oldest?",
+            desc: "Liege-Bastogne-Liege, also know as La Doyenne (\"The Old Lady\"), has been running since 1892 and is 256km (160 miles) long.",
+            choices:
+                [{text: "Milan-San Remo"},
+                    {text: "Tour of Flanders"},
+                    {text: "Paris-Roubaix"},
+                    {
+                        text: "Liege-Bastogne-Liege",
+                        isCorrect: true
+                    },
+                    {text: "Giro di Lombardia"}
+                ]
+        },
+        {
+            id: 3,
+            text: "What cyclist has won the most Monuments during their career?",
+            desc: "The Belgin cyclist Eddy Merckx won 19 Momuments and is considered the greatest of all times. He won: 7-Milan-San Remo, 2-Tour of Flanders, 3-Paris-Roubaix, 5-Liege-Bastogne-Liege, 2-Giro di Lombardia",
+            choices:
+                [{text: "Fausto Coppi"},
+                    {
+                        text: "Eddy Merckx",
+                        isCorrect: true
+                    },
+                    {text: "Rik Van Looy"},
+                    {text: "Roger De Vlaeminck"},
+                    {text: "Sean Kelly"}
+                ],
+            img: "./images/merckxWinning.jpg"
+        },
+        {
+            id: 4,
+            text: 'Which Monument is also known as "The Hell of the North"',
+            desc: 'Paris-Roubaix, which has run since 1896 in Northern France, is also called the "Hell of the North" because of its brutally rough cobbled roads and often horrific weather.',
+            choices:
+                [{text: "Milan-San Remo"},
+                    {text: "Tour of Flanders"},
+                    {
+                        text: "Paris-Roubaix",
+                        isCorrect: true
+                    },
+                    {text: "Liege-Bastogne-Liege"},
+                    {text: "Giro di Lombardia"}
+                ]
+        },
+        {
+            id: 5,
+            text: 'Which country does NOT host one of cycling\'s Monuments?',
+            desc: 'Germany does not host a Monument. Italy and Belgium both host 2 and France host 1.',
+            choices:
+                [{
+                    text: "Germany",
+                    isCorrect: true
+                },
+                    {text: "Italy"},
+                    {text: "France"},
+                    {text: "Belgium"}
+                ]
+        }
+    ],
+    answers: [],
+    winningImg: [
+        {
+            src: "./images/jungelsWin.jpg",
+            alt: "Jungels Win"
+        },
+        {
+            src: "./images/merckxWin.jpg",
+            alt: "Merckx Win"
+        },
+        {
+            src: "./images/valverdeWin.jpg",
+            alt: "Valverde Win"
+        }
+    ],
+    losingImg: [{src: "./images/gustavCrash.jpg",
+        alt: "Gustav Crash"}
+    ]
+}
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,182 @@
+'use-strict';
+
+function generateQuestions() {
+    let idx = 0;
+
+    if (QUIZ_DATA.answers.length > 0) {
+        idx = QUIZ_DATA.answers.length;
+    }
+
+    if (idx < QUIZ_DATA.questions.length) {
+        updateQuestionCount();
+
+        // generate radio button choices for the current question
+        const radioButtonChoices = QUIZ_DATA.questions[idx].choices.map(q => {
+            return `<label class="answerOption">
+                   <input type="radio" value="${q.isCorrect}" name="choice" required>
+                   <span>${q.text}</span>
+                 </label>`
+
+        }).join("\n");
+
+        return `<form id="question-form">
+                  <fieldset>
+                    <legend>${QUIZ_DATA.questions[idx].text}</legend>
+                    ${radioButtonChoices}
+                    <button type="submit" class="submitButton">Attack</button>
+                  </fieldset>
+                </form>`
+    } else {
+        generateFinalScore();
+    }
+}
+
+function startQuiz() {
+    $('.start-screen').on('click', '.start-button', function (event) {
+        $('.start-screen').remove();
+
+        $('.question-choices').css('display', 'flex');
+        $('.question-score').css('visibility', 'visible');
+
+        updateQuestionCount();
+    });
+}
+
+function renderQuestion() {
+    $('.question-choices').html(generateQuestions())
+}
+
+function updateQuestionCount(reset = false) {
+    if (!reset) {
+        let count = QUIZ_DATA.answers.length + 1;
+        $('#questionCount').text(count +"/" +QUIZ_DATA.questions.length);
+    } else {
+        $('#questionCount').text("0"+"/"+QUIZ_DATA.questions.length);
+    }
+
+}
+
+function updateScore(reset = false) {
+    if (!reset) {
+        $('#score').text(QUIZ_DATA.answers.filter(q => {
+            return q === "correct";
+        }).length);
+    } else {
+        $('#score').text(0);
+    }
+}
+
+// a answer chose has been select. Determine correct
+// or wrong, generate resonse and update score
+function userSelectAnswer() {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        if (answer !== 'undefined' && answer) {
+            QUIZ_DATA.answers.push('correct');
+            generateAnswerFeedback('correct');
+        } else {
+            QUIZ_DATA.answers.push('wrong');
+            generateAnswerFeedback('wrong');
+        }
+
+        if (QUIZ_DATA.answers.length === QUIZ_DATA.questions.length){
+            $('.nextButton').text("Finish Line");
+        }
+    });
+}
+
+function generateFinalScore() {
+    let feedback = "";
+    let src = "";
+    let alt = "final score picture";
+    let score = $("#score").text();
+
+    if (score == 5) {
+        feedback = "Congratulations! You crushed it and won!"
+        src = "./images/merckxPodium.jpg";
+    } else if (score >= 2 && score <= 4) {
+        feedback = "Bittersweet! You were so close to the win but just missed out!"
+        src = "./images/runnerup.jpg";
+    } else {
+        feedback = "Better luck next time. You were dropped from the race and did not finish."
+        src = "./images/bauerLost.jpg";
+    }
+
+    $('.question-choices').html(
+        `<div class="final-feedback">
+          <p>"${feedback}"</p>
+          <img src="${src}" alt="${alt}"/>
+          <button class="restart" type="submit">Restart</button>
+         </div>`
+    );
+
+    restart();
+}
+
+function restart() {
+    $(document).on("click", ".restart", function () {
+
+        QUIZ_DATA.answers = [];
+        updateQuestionCount(true);
+        updateScore(true);
+
+        location.reload();
+    });
+}
+
+function generateAnswerFeedback(result) {
+    let idx = 0;
+    let src = "";
+    let alt = "";
+    let feedback = "";
+
+    // get decsription explaning correct answer
+    if (QUIZ_DATA.answers.length > 0) {
+        idx = QUIZ_DATA.answers.length - 1;
+    }
+
+    // set correct or wrong image and alt
+    if (result === "correct") {
+        alt = "Bob Jungles winning";
+        //src="./images/merckxWinning.jpg";
+        src = './images/jungelsWin.jpg';
+        feedback = "Winning!!!";
+    } else {
+        alt = "Gustav Larsen Crashing";
+        src = './images/gustavCrash.jpg';
+        feedback = "Losing!!!";
+    }
+
+    updateScore();
+
+    // generate html for answer reveal
+    $('.question-choices').html(
+        `<div class="feedback">
+          <p>"${feedback}"</p>
+          <p>${QUIZ_DATA.questions[idx].desc}</p>
+          <img src="${src}" alt="${alt}"/>
+          <button type=button class="nextButton">Take Another Pull</button>
+          </div>`
+    );
+}
+
+//what happens when the user clicks next
+function generateNextQuestion() {
+    $('main').on('click', '.nextButton', function (event) {
+        renderQuestion();
+        userSelectAnswer();
+    });
+}
+
+function createQuiz() {
+    startQuiz();
+    renderQuestion()
+    generateNextQuestion();
+    userSelectAnswer();
+}
+
+$(createQuiz);
+
+
